a8ff690b18be177d1fe9680cddbdc9fbe996850b
SAV <staritsyn@mail.ru>
2025-06-04 14:01:33 +0700

    Начало 3

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 from aiogram.types import Message
 
-BOT_TOKEN = '5995958291:AAEZ1cN7M4vizQJLdHYYKsBZeAwN_N_uwJs'
+BOT_TOKEN = ''
 
 # Создаём объекты бота и диспетчера
 bot = Bot(token=BOT_TOKEN)
@@ -15,3 +15,20 @@ async def process_start_command(message: Message):
         'Привет! '
         'Меня зовут Эхо-бот!'
     )
+
+# Этот хендлер будет срабатывать на команду "/help"
+@dp.message(Command(commands=['help']))
+async def process_help_command(message: Message):
+    await message.answer(
+        'Напиши мне что-нибудь в ответ '
+        'я пришлю тебе твоё сообщение'
+    )
+
+# Этот хендлер будет срабатывать на любые ваши текстовые сообщения,
+# кроме команд "/start" "/help"
+@dp.message()
+async def send_echo(message: Message):
+    await message.reply(text=message.text)
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
