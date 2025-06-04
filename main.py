9605268edde47b313c1e9490299c70f204ade839
SAV <staritsyn@mail.ru>
2025-06-04 13:40:15 +0700

    Начало 1

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-print("Hello!")
-print("Hello, World!")
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+
+BOT_TOKEN = '5995958291:AAEZ1cN7M4vizQJLdHYYKsBZeAwN_N_uwJs'
+
+# Создаём объекты бота и диспетчера
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+# Этот хэндлер будет срабатывать на кнопку "/start"
+@dp.message(Command(commands=["start"]))
+async def process_start_command(message: Message):
+    await message.answer(
+        'Привет! '
+        'Меня зовут Эхо-бот!'
+    )
