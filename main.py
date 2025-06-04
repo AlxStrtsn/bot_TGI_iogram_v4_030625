a05d0054e22d2fb47f36fb15b87baff15e8523b1
SAV <staritsyn@mail.ru>
2025-06-04 16:24:31 +0700

    # Регистрация хендлеров

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ import os
 from pyexpat.errors import messages
 
 #TODO ловить ошибку в блоке с чтением файла токина
+#TODO пропускать только текст
+#TODO файл с логами
+
 
 f = open('.//gitignor//tok.txt', 'r')
 BOT_TOKEN = f.read()
@@ -16,26 +19,25 @@ bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
 # Этот хэндлер будет срабатывать на кнопку "/start"
-@dp.message(Command(commands=["start"]))
 async def process_start_command(message: Message):
-    await message.answer(
-        f'Здравствуйте, {message.chat.first_name} ({message.chat.username})!'
-    )
+    await message.answer(f'Здравствуйте, {message.chat.first_name} ({message.chat.username})!')
 
 # Этот хендлер будет срабатывать на команду "/help"
-@dp.message(Command(commands=['help']))
 async def process_help_command(message: Message):
     await message.answer(
         'Напиши мне что-нибудь в ответ '
         'я пришлю тебе твоё сообщение'
     )
 
-# Этот хендлер будет срабатывать на любые ваши текстовые сообщения,
+# Этот хендлер будет срабатывать на любые текстовые сообщения,
 # кроме команд "/start" "/help"
-@dp.message()
 async def send_echo(message: Message):
     await message.reply(text=message.text)
 
-# print(Message.text)
+# Регистрация хендлеров
+dp.message.register(process_start_command, Command(commands='start'))
+dp.message.register(process_help_command, Command(commands='help'))
+dp.message.register(send_echo)
+
 if __name__ == '__main__':
     dp.run_polling(bot)
